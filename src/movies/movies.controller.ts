f658c792fc7d0edabac9acb8ecca43a51d5fc2e0
call5h5ong <call5h5ong@gmail.com>
2021-04-05 12:53:30 +0900

    2.1 More Routes

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MoviesController {
@@ -6,23 +15,35 @@ export class MoviesController {
   getAll() {
     return 'This will return all movies.';
   }
-  @Get('/:id')
+
+  // search가 getOne보다 아래 있으면 getOne이 먼저 실행됨.
+  // 그래서 "search"를 id로 인식하는 불상사가 생길 수 있으니
+  // search는 getOne보다 위에 있어야 함.
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie with a title: ${searchingYear}`;
+  }
+
+  @Get(':id')
   getOne(@Param('id') movieId: string) {
     return `This will return one movie with the id ${movieId}`;
   }
 
   @Post()
-  create() {
-    return `This will create a movie`;
+  create(@Body() movieData) {
+    return movieData;
   }
 
-  @Delete('/:id')
+  @Delete(':id')
   remove(@Param('id') movieId: string) {
     return `This will delete a movie with the id: ${movieId}`;
   }
 
-  @Patch('/:id')
-  patch(@Param('id') movieId: string) {
-    return `This will patch a movie with the id: ${movieId} `;
+  @Patch(':id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updateMovie: movieId,
+      ...updateData,
+    };
   }
 }
