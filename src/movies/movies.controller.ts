113d8271ae19b21b16504a2a34b86680b99e1b17
call5h5ong <call5h5ong@gmail.com>
2021-04-08 12:31:13 +0900

    2.5 DTOs and Validation part Two
    타입들을 다양한 방식으로 다루기 위한 "@nestjs/mapped-types"를 설치함.

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -9,6 +9,7 @@ import {
   Query,
 } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
 
@@ -45,7 +46,7 @@ export class MoviesController {
   }
 
   @Patch(':id')
-  patch(@Param('id') movieId: number, @Body() updateData) {
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
     this.moviesService.update(movieId, updateData);
   }
 }
