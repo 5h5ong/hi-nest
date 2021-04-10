38ab05d1ccc01e2065e996584f8a148308322c52
call5h5ong <call5h5ong@gmail.com>
2021-04-10 23:09:41 +0900

    4.1 Testing GET movies id

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication, RequestTimeoutException } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
+    /*
+     * transform을 적용하지 않음 -> quary의 값이 string으로 들어옴
+     * Be Careful : 테스트에서도 실제 앱의 환경을 그대로 적용시켜야 함
+     */
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        // 검증되지 않은 property는 받지 않음
+        whitelist: true,
+        // 검증되지 않은 property는 받지도 않고 아예 경고까지 띄워버림
+        forbidNonWhitelisted: true,
+        // 들어온 string을 적절한 타입으로 변환
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -39,5 +53,16 @@ describe('AppController (e2e)', () => {
     it('DELETE', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
     });
+  });
+
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/999').expect(404);
+    });
+    it.todo('DELETE');
+    it.todo('PATCH');
   });
 });
