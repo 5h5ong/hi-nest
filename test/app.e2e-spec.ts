33daf8c68aaa7c7357d2186caa744c5e136cd370
call5h5ong <call5h5ong@gmail.com>
2021-04-14 21:09:08 +0900

    4.2 Testing PATCH and DELETE movies id
    patch 404,400 & delete 404은 응용해서 만들어봄

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -40,7 +40,7 @@ describe('AppController (e2e)', () => {
     it('GET', () => {
       return request(app.getHttpServer()).get('/movies').expect(200).expect([]);
     });
-    it('POST', () => {
+    it('POST 201', () => {
       return request(app.getHttpServer())
         .post('/movies')
         .send({
@@ -49,6 +49,17 @@ describe('AppController (e2e)', () => {
           genres: ['test'],
         })
         .expect(201);
+    });
+    it('POST 400', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+          test: 'Test',
+        })
+        .expect(400);
     });
     it('DELETE', () => {
       return request(app.getHttpServer()).delete('/movies').expect(404);
@@ -62,7 +73,29 @@ describe('AppController (e2e)', () => {
     it('GET 404', () => {
       return request(app.getHttpServer()).get('/movies/999').expect(404);
     });
-    it.todo('DELETE');
-    it.todo('PATCH');
+    it('PATCH 200', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Title' })
+        .expect(200);
+    });
+    it('PATCH 404', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/999')
+        .send({ title: 'Updated Title' })
+        .expect(404);
+    });
+    it('PATCH 400', () => {
+      return request(app.getHttpServer())
+        .patch('/movies/1')
+        .send({ title: 'Updated Title', justTest: 'Test' })
+        .expect(400);
+    });
+    it('DELETE 200', () => {
+      return request(app.getHttpServer()).delete('/movies/1').expect(200);
+    });
+    it('DELETE 404', () => {
+      return request(app.getHttpServer()).delete('/movies/9999').expect(404);
+    });
   });
 });
